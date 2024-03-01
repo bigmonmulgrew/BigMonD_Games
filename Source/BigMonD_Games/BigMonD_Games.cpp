818448864e256da5a2f97740bee0171c192cd1c2
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-03-01 09:46:51 +0000

    Initial commit

---
+++ Source/BigMonD_Games/BigMonD_Games.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BigMonD_Games.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BigMonD_Games, "BigMonD_Games" );
---
+++ Source/BigMonD_Games/BigMonD_GamesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BigMonD_GamesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIGMOND_GAMES_API ABigMonD_GamesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
