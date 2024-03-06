00550e0335be34ee10b1439b8d7e7a4b7a683947
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-03-06 16:48:16 +0000

    Added ball

---
+++ Source/BigMonD_Games/PongBall.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PongBall.h"
+#include "PaperSpriteComponent.h"
+
+// Sets default values
+APongBall::APongBall()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	MySprite = CreateDefaultSubobject<UPaperSpriteComponent>(TEXT("My Image"));
+	RootComponent = MySprite;
+}
+
+// Called when the game starts or when spawned
+void APongBall::BeginPlay()
+{
+	Super::BeginPlay();
+
+	MyVelocity = FVector(300, 0,333);
+	
+}
+
+// Called every frame
+void APongBall::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	SetActorLocation((GetActorLocation()+MyVelocity));
+
+}
+
