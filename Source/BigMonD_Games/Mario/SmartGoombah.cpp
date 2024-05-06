5a31f6a753cf92fea67b40b429300110b680a904
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-05-06 06:52:18 +0100

    Revert "broken goombah"
    This reverts commit 7618d0897c1d4aeb7c9a33995df0c735d70a51b1.

--- Source/BigMonD_Games/Mario/SmartGoombah.cpp
+++ Source/BigMonD_Games/Mario/SmartGoombah.cpp
@@ -2,35 +2,4 @@
 
 
 #include "BigMonD_Games/Mario/SmartGoombah.h"
-#include "Components/BoxComponent.h"
-
-
-ASmartGoombah::ASmartGoombah()
-{
-	LeftHoleDetector = CreateDefaultSubobject<UBoxComponent>(TEXT("Left Hole Detector"));
-	RightHoleDetector = CreateDefaultSubobject<UBoxComponent>(TEXT("Right Hole Detector"));
-
-	LeftHoleDetector->SetupAttachment(RootComponent);
-	RightHoleDetector->SetupAttachment(RootComponent);
-}
-
-void ASmartGoombah::BeginPlay()
-{
-	Super::BeginPlay();
-	
-	LeftHoleDetector->OnComponentEndOverlap.AddDynamic(this, &ASmartGoombah::LeftEndOverlap);
-	RightHoleDetector->OnComponentEndOverlap.AddDynamic(this, &ASmartGoombah::RightEndOverlap);
-}
-
-void ASmartGoombah::LeftEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	if(OtherActor->Tags.Contains("Floor")) WalkingDirection = 1;
-}
-
-void ASmartGoombah::RightEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
-	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
-{
-	if(OtherActor->Tags.Contains("Floor")) WalkingDirection = -1;
-}
 
