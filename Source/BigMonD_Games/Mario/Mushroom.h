8776fa404939036d9706619cf5cf0f4c5a535da1
David Mulgrew <bigmonmulgrew@users.noreply.github.com>
2024-05-07 22:08:22 +0100

    finished breakable block

---
+++ Source/BigMonD_Games/Mario/Mushroom.h
@@ -0,0 +1,17 @@
+// Copyright 100705789
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BigMonD_Games/Mario/Collectible.h"
+#include "Mushroom.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BIGMOND_GAMES_API AMushroom : public ACollectible
+{
+	GENERATED_BODY()
+	
+};
